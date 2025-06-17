501a4a2946010881402e4dfafe23095775750dc0
V4l3r14n3 <vale.mantilla15@gmail.com>
2025-06-17 11:58:12 -0500

    Fix port error with start.sh script

--- start.sh
+++ start.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-# Reemplaza el puerto en Apache con la variable $PORT de Railway
-sed -i "s/80/${PORT}/g" /etc/apache2/ports.conf
-sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf
+# Verifica si PORT está definido
+if [ -z "$PORT" ]; then
+  echo "ERROR: La variable PORT no está definida."
+  exit 1
+fi
 
-# Inicia Apache en primer plano
+# Reescribe puertos en Apache
+echo "Escuchando en el puerto: $PORT"
+echo "Listen ${PORT}" > /etc/apache2/ports.conf
+
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:${PORT}>
+    DocumentRoot /var/www/html
+
+    <Directory /var/www/html>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+# Inicia Apache
 apache2-foreground
